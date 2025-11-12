6157d64e0daee5bd2494e0b081a41c08df937dbd
Erdemtugs <erdemtugs515@gmail.com>
2025-11-12 11:16:46 +0200

    11/12/2025

---
+++ Week 3/Task 1, Week 3.py
@@ -0,0 +1,12 @@
+print("Program starting.")
+print("Please input 2 integers.")
+int1 = input("Insert first integer: ")
+int2 = input("Insert second integer: ")
+print("Comparing inserted integers")
+
+if int1 > int2:
+    print ("Integer 1 is greater.")
+if int1 == int2:
+    print("Both integers are equal.")
+if int1 < int2:
+    print ("Integer 2 is greater.")
